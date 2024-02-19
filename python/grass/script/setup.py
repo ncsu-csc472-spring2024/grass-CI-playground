0f45c888956ea8366660b16eae47f9f862a55adb
Ntp9413 <156957724+Ntp9413@users.noreply.github.com>
2024-02-18 19:59:56 -0500

    Modified file name to match existing naming convention

--- python/grass/script/setup.py
+++ python/grass/script/setup.py
@@ -97,11 +97,11 @@ VERSION_MINOR = "@GRASS_VERSION_MINOR@"
 
 def write_gisrc(dbase, location, mapset):
     """Write the ``gisrc`` file and return its path."""
-    with tmpfile.NamedTemporaryFile(mode="w", delete=False) as tmp:
-        gisrc = tmp.name
-        tmp.write("GISDBASE: %s\n" % dbase)
-        tmp.write("LOCATION_NAME: %s\n" % location)
-        tmp.write("MAPSET: %s\n" % mapset)
+    with tmpfile.NamedTemporaryFile(mode="w", delete=False) as rc:
+        gisrc = rc.name
+        rc.write("GISDBASE: %s\n" % dbase)
+        rc.write("LOCATION_NAME: %s\n" % location)
+        rc.write("MAPSET: %s\n" % mapset)
     return gisrc
 
 
